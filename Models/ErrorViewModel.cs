d4a2885eef0d1933053c0da621b661dc0e619a0a
Huy <quanghuy2752000@gmail.com>
2021-12-15 13:27:50 +0700

    first_commit_VQH013

---
+++ Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace VuQuangHuy013.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}

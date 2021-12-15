e8eef566a9a6018e88330fdbc18cdc4bae5ff2b4
Huy <quanghuy2752000@gmail.com>
2021-12-15 14:05:37 +0700

    Final_commit_VQH013

---
+++ Models/StringProcessVQH013.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace VuQuangHuy013.Models
+{
+    public class StringProcessVQH013
+    {
+        public String LowerToUpper(string name) {
+            return name.ToUpper();
+        }
+    }
+}
